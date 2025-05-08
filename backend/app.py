0fa78924f70cb42d6abb272aa4be9eda2a207538
Cheran <148336982+cheran99@users.noreply.github.com>
2025-05-08 10:35:23 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/health")
+def health_check():
+    return "Backend is running"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
